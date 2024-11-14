29122a892e36f197478e0ed9988122e79c8b7e58
BehruzWeb <behruz33365@gmail.com>
2024-11-14 16:40:32 +0500

    Behruzbek

--- src/components/Behruzbek/Behruzbek.jsx
+++ src/components/Behruzbek/Behruzbek.jsx
@@ -1,6 +1,48 @@
+import "./Behruzbek.css"
 const Behruzbek = () => {
  return(
-  <div>Behruzbek</div>
+  <div>
+  <div className='happyclients'>
+   <h1>Happy Clients Say</h1>
+   <div className="clientes">
+<div className="card-1">
+ <div className="user-1">
+  <img src={"https://s3-alpha-sig.figma.com/img/b7c1/402a/532c38234a6784a37ad06b30a1db357f?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MpiF21YlG8QTUF7dmdjPJnod9NAqAQEFFnJ4PI~XY7fLy3S2EjEkDQnV6r73KeEMUueWc-AmLerLwh2mvb0Q84nmVzHCKIInrHba9DLE6GtB5esnBLhiTi8ao72wGVzEGX-WHC6yHJZyfpob~p7e4o2mLmMq42RRbIq3YmIIBID4bKqX-LKUNJFim0XC9GTrUIysb35wJpMEv-7zI6~-ozH8H3GCOwWDpPbtJ6u6AMh~h3OAuRvM5JNEbNVldvK7TDgdCMARDT4QngKgIgyvYS1zwFfC7XP4-7-sQnZwtf81WJ-XpZEuYAhzO39RN6X-qV-zUF81tNCcYwUnRXWk-A__"} alt="" />
+  <div className="p">
+  <h3>KHALI NAZIR</h3>
+  <p>CEO of ULDesk</p>
+  </div>
+ </div>
+ <div className="lorem">
+ <p>Thirty for remove plenty regard you summer though. He preference Partiality on or continuing in particular principles as. Do believing oh disposing to supported allowance we.</p>
+</div>
+</div>
+<div className="card-2">
+ <div className="user-2">
+  <img src={"https://s3-alpha-sig.figma.com/img/8e5f/5c51/427a1f62a2d277788a853d203733f8d5?Expires=1732492800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=R7fGktH6CuYldS2zy57fAon0CmN-gQNDzltHlpzTblnIkU3v2qt-v-oqWeIhH5HtjUOd0b77l3W-mRiheIO0WbsXSEcNnCKbdtTtbBA8X0BC44OqSiiRpquwxV6sL0sn88nX~7pY9KcIq7TiAj6m-t3E5m0RJK12i7FKgvubIJOI4EPnoJqWFuL5RA0Jw8U9QcimjW4mBgjLkhOYEdvR1eRIvI1b2IBoWtx0YnZvxd7iwSD-26IHexzErOovAxXC7u~Z2m4PLNpcz2wXncG016Zwi5fzkIPGwahZ0cTFbHuQHlYgO5kE0-yTW73~iLqzngcN65HOfDnmkwLhXpql1A__"} alt="" />
+ <div className="p">
+  <h3>KHALI NAZIR</h3>
+  <p>CEO of ULDesk</p>
+  </div>
+ </div>
+ <div className="lorem">
+ <p>Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. Merits behind on afraid or warmly.
+ </p>
+</div>
+</div>
+<div className="icons"></div>
+   </div>
+
+  </div>
+  <div className="subscribe">
+   <h1>Subscribe to the Newsletter</h1>
+   <p>Enter your email below to get our daily offers and news</p>
+   <div className="button">
+      <input type="text"  placeholder='Enter your email.'/>
+      <button>Get Sarted</button>
+   </div>
+  </div>
+  </div>
  )
 }
 
